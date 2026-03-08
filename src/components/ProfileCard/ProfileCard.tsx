b2378d36b3094c181735042b9ec311faf061b1a6
shin1488 <syc1488@naver.com>
2026-03-08 19:37:26 +0900

    feat : 코멘트 컴포넌트 css 수정

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,8 +1,20 @@
+import image from '../../assets/react.svg';
+
 const ProfileCard = () => {
   return (
     <div
       className={`bg-card-background flex h-75 w-75 flex-col gap-4 rounded-lg p-8`}
-    ></div>
+    >
+      <div
+        className={`flex flex-row items-center justify-start gap-4 bg-transparent`}
+      >
+        <img
+          src={image}
+          className={`bg-primary h-10 w-10 shrink-0 rounded-4xl`}
+        />
+        <p className={`w-20 shrink-0 text-start`}>닉네임</p>
+      </div>
+    </div>
   );
 };
 
