b86b80e0b7e30fd050fffb17e5fd499bfb64b20d
shin1488 <syc1488@naver.com>
2026-03-09 16:24:42 +0900

    feat : 측면 shelf 에셋 추가 및 grid 배치 완료

--- src/components/Shelf/ShelfTest.tsx
+++ src/components/Shelf/ShelfTest.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import { useState } from 'react';
 import useShelfSelection from '../../hooks/useShelfSelection';
 import BackSlots from './BackSlots';
 import Dividers from './Dividers';
@@ -24,12 +26,25 @@ const ShelfTest = () => {
   const gridCols: Array<number> = Array.from({ length: 4 });
   const dividerRows: Array<number> = Array.from({ length: 3 });
 
+  const [isShrinked, setIsShrinked] = useState(true);
+
   return (
     <div
-      className={`h-130 w-125 shrink-0 bg-[url('/src/assets/shelf_front.png')] bg-cover bg-center bg-no-repeat px-14 pt-15 pb-21`}
+      className={clsx(
+        `bg-no-repeattransition-all shrink-0 bg-cover bg-center duration-700 ease-out transform-3d`,
+        isShrinked
+          ? "aspect-1804/2040 h-100 bg-[url('/src/assets/shelf_side.png')] pt-15.5 pr-17 pb-6"
+          : "aspect-1208/1257 h-125 bg-[url('/src/assets/shelf_front.png')] px-14 pt-15 pb-21"
+      )}
+      style={{ perspective: '1000px' }}
     >
       <div
-        className={`relative grid h-full w-full shrink-0 grid-cols-4 grid-rows-[1fr_auto_1fr_auto_1fr_auto_1fr] select-none`}
+        className={clsx(
+          `relative grid h-full w-full shrink-0 grid-cols-4 grid-rows-[1fr_auto_1fr_auto_1fr_auto_1fr] select-none`,
+          isShrinked
+            ? 'transform-[translateZ(100px)_translateX(-4px)_skewY(13.8deg)_scale(0.7)]'
+            : 'transform-[translateZ(0px)_rotateY(0deg)_rotateX(0deg)_scale(1.0)]'
+        )}
         onMouseLeave={handleMouseUp}
         onMouseUp={handleMouseUp}
       >
