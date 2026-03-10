c98a352634103fca3fb2d3f8d933c6e7e65c50cb
shin1488 <syc1488@naver.com>
2026-03-10 09:44:46 +0900

    feat : 벽지 임시 완성

--- src/containers/RoomContainer/RoomContainer.tsx
+++ src/containers/RoomContainer/RoomContainer.tsx
@@ -7,27 +7,22 @@ interface RoomContainerProps {
 }
 
 const RoomContainer = ({ isHome }: RoomContainerProps) => {
-  // Home일 시 약간의 dim 효과
-  const bgDim = isHome
-    ? `bg-[linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('/src/assets/image.jpg')]`
-    : `bg-[url('/src/assets/image.jpg')]`;
-
   return (
     <div
       className={clsx(
-        `relative flex h-full w-full items-center justify-center`,
-        bgDim
+        `relative z-0 flex h-250 w-full min-w-7xl shrink-0 items-center justify-center bg-cover bg-bottom bg-no-repeat`,
+        isHome
+          ? `bg-[linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('/src/assets/floor.png')]`
+          : `bg-[url('/src/assets/floor.png')]`
       )}
     >
-      {/* Home이 아닐 시 로고 미표시 */}
-      {isHome && (
-        <img
-          src={logo}
-          className={`absolute z-0 w-100 items-center justify-center object-contain`}
-        />
-      )}
-      {/* 선반 포함한 룸 섹션 */}
-      <main className={`z-10 flex max-w-7xl min-w-[70vw] justify-between`}>
+      <div
+        className={`bg-card-background absolute top-[-24.5%] right-1/2 z-10 h-[75%] w-1/2 shrink-0 origin-right transform-[skewY(-13.8deg)] bg-[url('/src/assets/kaede.png')] bg-contain bg-center bg-no-repeat`}
+      ></div>
+      <div
+        className={`bg-card-background absolute top-[-24.5%] left-1/2 z-10 h-[75%] w-1/2 shrink-0 origin-left transform-[skewY(13.8deg)] bg-[url('/src/assets/kaede.png')] bg-contain bg-center bg-no-repeat`}
+      ></div>
+      <main className='absolute z-20 flex w-full max-w-7xl justify-between px-20'>
         <Shelf isLeft={true} />
         <Shelf isLeft={false} />
       </main>
