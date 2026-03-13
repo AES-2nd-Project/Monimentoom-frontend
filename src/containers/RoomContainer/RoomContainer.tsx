27d7906d73aa8fd60352b8c576a2fe8d2881b6cc
shin1488 <syc1488@naver.com>
2026-03-13 19:16:26 +0900

    refactor : RoomContainer 로직과 레이아웃 분리

--- src/containers/RoomContainer/RoomContainer.tsx
+++ src/containers/RoomContainer/RoomContainer.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
@@ -9,7 +8,6 @@ import {
   updatePosition,
 } from '../../api/position-api';
 import { getRoomMain } from '../../api/room-api';
-import Shelf from '../../components/Shelf/Shelf';
 import type { AppDispatch, RootState } from '../../store';
 import {
   setShelfItems,
@@ -17,6 +15,7 @@ import {
 } from '../../store/shelfSlice';
 import type { PositionResponse, WallSide } from '../../types/position';
 import type { Item } from '../../types/room';
+import RoomScene from './RoomScene';
 
 // PositionResponse → Item 변환 (x=r1, y=c1), goodsId로 imageUrl 매핑
 const positionToItem = (
@@ -197,89 +196,7 @@ const RoomContainer = () => {
     prevEditMode.current = isEditMode;
   }, [isEditMode, syncPositions]);
 
-  return (
-    <div
-      className={clsx(
-        `relative z-0 flex h-250 w-full min-w-7xl shrink-0 items-center justify-center overflow-hidden`
-      )}
-    >
-      {/* 홈 화면 효과 */}
-      {isHome && (
-        <div
-          className={clsx(
-            'pointer-events-none z-100 flex h-full w-full items-center justify-center',
-            'bg-purple-black/50',
-            'backdrop-blur-[6px]'
-          )}
-        >
-          <div className="aspect-1280/698 w-200 bg-[url('/src/assets/logo.png')] bg-cover bg-center bg-no-repeat opacity-80"></div>
-        </div>
-      )}
-
-      {/* 바닥 */}
-      <div
-        className={`absolute top-1/2 z-0 h-1/2 w-7xl bg-[url('/src/assets/floor.jpg')] bg-size-[101%_cover] bg-position-[calc(50%-6px)] bg-no-repeat`}
-      ></div>
-
-      {/* 벽 */}
-      <div
-        className={`absolute top-[-25%] right-1/2 z-10 h-[90%] w-160 shrink-0 origin-right transform-[skewY(-13.8deg)] border-r-2 border-b-6 bg-[url('/src/assets/wall.jpg')] bg-cover bg-center bg-no-repeat`}
-      ></div>
-
-      {/* 벽 */}
-      <div
-        className={`absolute top-[-25%] left-1/2 z-10 h-[90%] w-160 origin-left transform-[skewY(13.8deg)] border-b-6 border-l-2 bg-[url('/src/assets/wall.jpg')] bg-cover bg-center bg-no-repeat`}
-      ></div>
-
-      {/* 액자 */}
-      <div
-        className={clsx(
-          'absolute bottom-[68%] left-1/2 z-20 aspect-1766/1703 w-70',
-          'origin-right -translate-x-[calc(100%+20px)] -skew-y-[13.8deg]'
-        )}
-      >
-        <div
-          className={`absolute inset-0 z-20 bg-[url('/src/assets/frame.png')] bg-cover bg-no-repeat`}
-        ></div>
-        <div
-          className={`bg-purple-white absolute top-23 left-5 z-10 flex h-[59%] w-[87%] origin-right items-center justify-center bg-[url('/src/assets/Kaede.png')] bg-contain bg-center bg-no-repeat`}
-        ></div>
-      </div>
-
-      {/* 책장 */}
-      <div
-        className={clsx(
-          'absolute bottom-[58%] left-1/2 z-20 aspect-1600/1868 w-70',
-          'origin-right -translate-x-150 -skew-y-[1.8deg]',
-          'bg-[url("/src/assets/bookshelf.png")] bg-cover bg-center bg-no-repeat'
-        )}
-      ></div>
-
-      {/* 창문 */}
-      <div
-        className={clsx(
-          'absolute bottom-[62%] left-1/2 z-20 aspect-1478/1621 w-80',
-          'origin-left translate-x-37.5 skew-y-[13.8deg]',
-          'bg-[url("/src/assets/window.png")] bg-cover bg-center bg-no-repeat'
-        )}
-      ></div>
-
-      {/* 이젤 */}
-      <div
-        className={clsx(
-          'absolute bottom-[10%] left-1/2 z-20 aspect-1349/2010 w-80',
-          'origin-bottom -translate-x-41.25',
-          "bg-[url('/src/assets/easel.png')] bg-cover bg-center bg-no-repeat"
-        )}
-      ></div>
-
-      {/* 선반 */}
-      <main className='absolute bottom-40 z-20 flex w-full max-w-7xl origin-bottom justify-between px-20'>
-        <Shelf isLeft={true} />
-        <Shelf isLeft={false} />
-      </main>
-    </div>
-  );
+  return <RoomScene isHome={isHome} />;
 };
 
 export default RoomContainer;
